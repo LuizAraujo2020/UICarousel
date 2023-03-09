dafca81659ca20c0b3cadbd0788267be714f6fdb
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-03-09 17:25:23 -0300

    Initial Commit

---
+++ UICarousel/UICarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  UICarouselApp.swift
+//  UICarousel
+//
+//  Created by Luiz Araujo on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct UICarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
